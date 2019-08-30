d6ec20d2dd4928c83a907cc7b61eea15ac3cc3dd
wyqBird <1049830812@qq.com>
2019-08-30 10:39:17 +0800

    update chapter04

---
+++ netty-book2/src/main/java/com/wyq/chapter04/stickysolve/TimeServerHandler.java
@@ -0,0 +1,33 @@
+package com.wyq.chapter04.stickysolve;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author Yongqian Wang
+ * @version 1.0
+ */
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        String body = (String) msg;
+        System.out.println("The time server receive order : " + body
+                + " ; the counter is : " + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
+                System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
